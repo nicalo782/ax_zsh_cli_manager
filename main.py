e4739b806470afb86854f29c6c83f844715877f9
nicalo782 <100510013+nicalo782@users.noreply.github.com>
2023-12-18 00:59:35 +0200

    Додано початковий функціонал для управління плагінами Zsh [Alpha 0.1.0]
    Додано функціонал управління плагінами для файлу конфігурації Zsh.
    Створено та оновлено файл VERSION.txt.
    Додано функції:
    - show_installed_plugins(): для відображення встановлених плагінів користувачу;
    - remove_zshrc_plugin(plugin): для видалення плагіну/плагінів з файлу конфігурації Zsh;
    - Оновлені read_zshrc_plugins() та add_zshrc_plugin(plugin): для завантаження та додавання плагінів з урахуванням збереженого шляху до .zshrc.
    
    Це версія [Alpha 0.1.0].

---
+++ main.py
@@ -0,0 +1,1 @@
+import install_zsh, zshrc_path_finder, zsh_plugin_manager
---
+++ zsh_plugin_manager.py
@@ -0,0 +1,23 @@
+import os
+zshrc_config_file = 'zshrc_path.txt'  # Назва файлу для збереження шляху до .zshrc
+def find_zshrc():
+    home_directory = os.path.expanduser("~")
+    zshrc_path = os.path.join(home_directory, '.zshrc')
+    if os.path.isfile(zshrc_path):
+        return zshrc_path
+    else:
+        print("Файл .zshrc не знайдено в домашній директорії.")
+        custom_path = input("Будь ласка, введіть шлях до файлу .zshrc: ")
+        return custom_path
+
+def save_zshrc_path():
+    zshrc_path = find_zshrc()
+    with open(zshrc_config_file, 'w') as config_file:
+        config_file.write(zshrc_path)
+
+def load_zshrc_path():
+    try:
+        with open(zshrc_config_file, 'r') as config_file:
+            return config_file.read().strip()
+    except FileNotFoundError:
+        return None
