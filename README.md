5cad277b3c0322c41bc6d1c01222ac5e04860a1f
nicalo782 <100510013+nicalo782@users.noreply.github.com>
2023-12-23 22:33:47 +0200

    Enhancement: Added imports, argument handling, and ASCII art to main.py
    Added imports and command-line argument handling to main.py
    Implemented ASCII art for program name, version, and author
    Added placeholder for an Easter egg feature
    Updated Readme.md
    Version [Alpha 0.1.1]

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # AX_Cli_zsh_manager
+English:
+Project Description:
+
+This project encompasses a set of functionalities focused on Zsh setup and management. It includes features such as installing Zsh and its dependencies, managing the Oh My Zsh framework and its dependencies, a plugin manager, and a configuration manager (for configuration files handling). The aim is to streamline the setup process and offer convenient tools for configuring and managing Zsh and its associated components. Functionality is subject to change!
+
+Version: Alpha 0.1.0
+
+Please note that the project is in its initial development phase and the described functionalities are planned for implementation in subsequent updates.
+
+Ukrainian:
+Опис проекту:
+
+Цей проект включає набір функцій, спрямованих на налаштування та управління Zsh. Він включає такі можливості, як встановлення Zsh та його залежностей, управління фреймворком Oh My Zsh та його залежностями, менеджер плагінів і менеджер конфігурації (для роботи з конфігураційними файлами). Мета полягає в спрощенні процесу налаштування та наданні зручних інструментів для налаштування та управління Zsh та його компонентами.
+
+Версія: Alpha 0.1.0
+
+Зверніть увагу, що проект перебуває на початковому етапі розробки, а описані функціональності планується впроваджувати у наступних оновленнях. Функціонал може змінюватися!
--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-import install_zsh, zshrc_path_finder, zsh_plugin_manager
+import install_zsh, zshrc_path_finder, zsh_plugin_manager, subprocess, os, platform, argparse, art
+
+# comments Lang: UA/ENG
+if __name__ == "__main__":
+    main()
+    print_ASCII_programm_name_art()
+    print_ASCII_version_art()
+def main():
+
+     # Створення об'єкту парсера/Create object parser
+    parser = argparse.ArgumentParser(description='Опис програми сокоро буде тут...')
+
+    # Додавання позиційного та опційного аргументів/Add positional and optional arguments
+    #parser.add_argument()
+    parser.add_argument('--print-author')
+    parser.add_argument('--print-version')
+
+    # Парсинг аргументів командного рядка/Parse the command-line arguments
+    args = parser.parse_args()
+
+    # Доступ до значень аргументів/Access the argument values
+    mode = args.mode
+
+def Easter_egg(): #Easter egg coming soon...
+    pass
+
+def print_ASCII_programm_name_art(): #Виводить назву програми артом з ASCII символів / Prints programm name ASCII symvols
+    art.tprint("AX_CLI_ZSH_Manager")
+
+def print_ASCII_version_art(): #Виводить версію програми артом з ASCII символів / Prints programm version ASCII symvols
+    art.tprint("v0.1.0")
+
+def print_ASCII_author_art(): #Виводить автора програми артом з ASCII символів / Prints programm author ASCII symvols
+    art.tprint("<nicalo786>")
